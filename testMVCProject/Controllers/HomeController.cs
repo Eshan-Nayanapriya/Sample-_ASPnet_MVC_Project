3675c50c98b5ad7729d8ff7c7a9ec5b424b11ed3
Eshan Nayanapriya <eshannayanapriyaprofessional@gmail.com>
2025-04-22 17:36:58 +0530

    add department

--- testMVCProject/Controllers/HomeController.cs
+++ testMVCProject/Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using Services.DepartmentServices;
+using Services.FacultyServices;
 using testMVCProject.Models;
 
 namespace testMVCProject.Controllers
@@ -9,17 +10,22 @@ namespace testMVCProject.Controllers
     {
         private readonly ILogger<HomeController> _logger;
         private readonly IDepartmentRepository _departmentServices;
+        private readonly IFacultyRepository _facultyServices;
 
-        public HomeController(ILogger<HomeController> logger, IDepartmentRepository departmentServices)
+        public HomeController(ILogger<HomeController> logger, IDepartmentRepository departmentServices, IFacultyRepository facultyRepository)
         {
             _logger = logger;
             _departmentServices = departmentServices;
+            _facultyServices = facultyRepository;
         }
 
         public IActionResult Index()
         {
             var res = _departmentServices.GetAll().Result;
-            return View(res);
+            ViewBag.Departmentlist = res;
+            ViewBag.FacultyList = _facultyServices.GetAll().Result;
+            ViewBag.Department = null;
+            return View();
         }
 
         public IActionResult Privacy()
@@ -31,6 +37,26 @@ namespace testMVCProject.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public async Task<IActionResult> add(Department details)
+        {
+            var addDepartment = new Department();
+            addDepartment.DepartmentName = details.DepartmentName; 
+            addDepartment.FacultyId = details.FacultyId;
+            await _departmentServices.Add(addDepartment);
+            return RedirectToAction("Index");
+        }
+
+        public async Task<IActionResult> Manage(int Id)
+        {
+            var resDepartment = await _departmentServices.GetById(Id);
+
+            var res = _departmentServices.GetAll().Result;
+            ViewBag.Departmentlist = res;
+            ViewBag.FacultyList = _facultyServices.GetAll().Result;
+            ViewBag.Department = resDepartment;
+            return View("Index");
         }
     }
 }
